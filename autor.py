2f539ac309859b3e5040a60c1eca8a288f8820ea
Nico Steppat <nico.steppat@caelum.com.br>
2011-05-14 16:33:39 -0300

    refactoring

--- autor.py
+++ autor.py
@@ -9,9 +9,9 @@ class Comando:
 
         def __init__(self, comandoArray):
 		self.p = subprocess.Popen(comandoArray, stdout=subprocess.PIPE)
-		self.__execute__()
+		self.__execute()
 
-	def __execute__(self):
+	def __execute(self):
 		if self.output is None:
 			self.output =self.p.communicate()[0]
 		return self.output
@@ -73,40 +73,49 @@ class Commit:
 	def __str__(self):
                 return self.shaHash
 
-	def create(commitString, statsString):
+class CommitsFactory:
+
+	def __clean_hash(self,rawHash):
+		return rawHash[1:len(rawHash[0])]
+
+	def __clean_msg(self,rawMsg):
+		return rawMsg[0:len(rawMsg)-1] 
+	
+	def create_commit(self, commitString, statsString):
 		commitValues=commitString.split('|')
 		commitStats=CommitStats.create(statsString)
 		return Commit(
-			shaHash = commitValues[0][1:len(commitValues[0])],
-			mensagem = commitValues[7][0:len(commitValues[7])-1], 
+			shaHash = self.__clean_hash(commitValues[0]),
+			mensagem = self.__clean_msg(commitValues[7]), 
 			autor = Pessoa(nome=commitValues[1], email=commitValues[2]), 
 			autorData = commitValues[3], 
 			committer = Pessoa(nome=commitValues[4], email=commitValues[5]), 
 			commitData = commitValues[6],
 			commitStats = commitStats)
 
-	create = staticmethod(create)
+	def git_log_as_array(self):
+		comando = Comando(['git', 'log', '--pretty=format:"%H|%an|%ae|%ai|%cn|%ce|%ci|%s"', "--shortstat", "--no-merges"])
+		return comando.saida().splitlines();
 
+	def commits_from_git_log(self):
+		logLines = self.git_log_as_array()
 
-comando = Comando(['git', 'log', '--pretty=format:"%H|%an|%ae|%ai|%cn|%ce|%ci|%s"', "--shortstat", "--no-merges"])
-saida = comando.saida().splitlines()
+		commits = list()
 
-commits = list()
+		while logLines:
+			firstLine = logLines.pop().strip()
+			#print "analyzing: " + firstLine + ", " + str(len(firstLine))
+			if not firstLine: 
+				#print "skiped: " + firstLine
+				firstLine = logLines.pop()
+			secondLine = logLines.pop();
+			#print "CommitStats:  " + firstLine
+			#print "CommitString: " + secondLine
+			commits.append(self.create_commit(commitString=secondLine, statsString=firstLine))
+		
+		return commits
 
-while saida:
-	#print "---------------------------"
-	commitStats=saida.pop().strip()
-	#print "analyzing: " + commitStats + ", " + str(len(commitStats))
-	if not commitStats: 
-		#print "skiped: " + commitStats
-		commitStats=saida.pop()
-	commitString=saida.pop();
-	#print "CommitString: " + commitString
-	#print "CommitStats:  " + commitStats
-	#print commitStats.split(',')[1]
-	commits.append(Commit.create(commitString, commitStats))
-
-for commit in commits:
-	print commit.mensagem 
+for commit in CommitsFactory().commits_from_git_log():
+	print "Msg: %s" % (commit.mensagem)
 
 	
